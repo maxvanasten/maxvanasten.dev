bc19557d626dfa8e48f986a50ab77318c548a30b
maxvanasten <maxvasten@gmail.com>
2024-04-01 14:42:03 +0200

    added -D and -P modes for local/production development

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -13,6 +14,18 @@ func main() {
 		http.HandleFunc(path, handler)
 	}
 
-	fmt.Println("http://localhost:3000")
-	http.ListenAndServe(":3000", nil)
+	if len(os.Args) > 1 {
+		mode := os.Args[1]
+		if mode == "-D" {
+			fmt.Println("Server running on: http://localhost:3000")
+			http.ListenAndServe(":3000", nil)
+		} else if mode == "-P" {
+			fmt.Println("Server running on: https://maxvanasten.dev")
+			http.ListenAndServeTLS(":443", "./domain.cert.pem", "./private.key.pem", nil)
+		} else {
+			fmt.Println("Unknown 'mode' option, try -D for local development or -P for production.")
+		}
+	} else {
+		fmt.Println("Please provide a 'mode' to run the server on, try -D for local development or -P for production.")
+	}
 }
